461925f9888e4eb193ec93d5b608039f17870cb4
NimmiW <nimmirashinika@gmail.com>
2018-03-13 13:10:57 +0530

    - second commit

--- WebApi4/Controllers/ValuesController.cs
+++ WebApi4/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WebApi4.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Love", "You baby <3" };
         }
 
         // GET api/values/5
