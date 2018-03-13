85a4af0bd4537d1ffd5581e6dca72c63d5a9f3d2
NimmiW <nimmirashinika@gmail.com>
2018-03-13 13:00:19 +0530

    - initial commit

---
+++ WebApi4/Controllers/TransactionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi4.Models;
+
+namespace WebApi4.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        private WebApi4Context db = new WebApi4Context();
+
+        // GET: api/Transactions
+        public IQueryable<Transaction> GetTransactions()
+        {
+            return db.Transactions;
+        }
+
+        // GET: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult GetTransaction(long id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transaction);
+        }
+
+        // PUT: api/Transactions/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTransaction(long id, Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != transaction.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(transaction).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TransactionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Transactions
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult PostTransaction(Transaction transaction)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Transactions.Add(transaction);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = transaction.Id }, transaction);
+        }
+
+        // DELETE: api/Transactions/5
+        [ResponseType(typeof(Transaction))]
+        public IHttpActionResult DeleteTransaction(long id)
+        {
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            db.Transactions.Remove(transaction);
+            db.SaveChanges();
+
+            return Ok(transaction);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TransactionExists(long id)
+        {
+            return db.Transactions.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApi4/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi4.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public DateTime EnteredDateTime { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string AccountName { get; set; }
+        public float Amount { get; set; }
+    }
+}
